71587161eef5d5449d923466cce11e0c4b3ceb53
Raitis Stengrevics <dagrevis@gmail.com>
2020-03-19 18:56:05 +0200

    Don't collapse deep items in outputs/console

--- src/outputs/console/index.ts
+++ src/outputs/console/index.ts
@@ -1,9 +1,17 @@
+import util from 'util'
+
 import _ from 'lodash'
 
 import { Output } from '../../types'
 
 const output: Output = () => async (job, results) => {
-  console.log(job.id, results)
+  console.log(
+    job.id,
+    util.inspect(results, {
+      depth: null,
+      colors: true,
+    }),
+  )
 }
 
 export default output
