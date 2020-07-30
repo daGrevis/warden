69955af5bd45708d1749b0c273545eb04b74c11a
Raitis Stengrevics <dagrevis@gmail.com>
2020-07-31 02:25:18 +0300

    facebook: Support regular text posts & photos

--- src/inputs/facebook/index.ts
+++ src/inputs/facebook/index.ts
@@ -1,4 +1,5 @@
 import _ from 'lodash'
+import { ElementHandle } from 'playwright'
 
 import { Input } from '../../types'
 import withBrowser from '../../withBrowser'
@@ -11,11 +12,75 @@ type Result = {
   id: string
   name: string
   url: string
+  imageUrl?: string
 }
 
 const HOST = 'https://www.facebook.com'
 
 const POSTS_SELECTOR = '._1xnd' // This will probably break any time now.
+
+const parseResult = async (
+  $post: ElementHandle,
+): Promise<Result | undefined> => {
+  const $timestamp = await $post.$('[data-testid="story-subtitle"] a')
+
+  if (!$timestamp) {
+    return undefined
+  }
+
+  let url = await $timestamp.evaluate(($) => ($ as HTMLAnchorElement).href)
+
+  const urlObject = new URL(url)
+
+  // Gets rid of query params.
+  url = urlObject.origin + urlObject.pathname
+
+  const segments = _.reject(
+    _.split(urlObject.pathname, '/'),
+    (segment) => segment === '' || segment === '/',
+  )
+
+  if (segments[0] === 'events') {
+    // TODO: Handle events.
+    return undefined
+  }
+
+  const id = _.last(segments)!
+
+  const $userContent = await $post.$('.userContent')
+
+  if (!$userContent) {
+    return undefined
+  }
+
+  let name = ''
+  const $textImage = await $userContent.$('span:nth-child(2) span')
+  if ($textImage) {
+    name = await $textImage.evaluate(($) => $.textContent!)
+  } else {
+    name = await $userContent.evaluate(($) => $.textContent!)
+  }
+
+  const $image = await $post.$('.mtm img')
+  let imageUrl = undefined
+  if ($image) {
+    imageUrl = await $image.evaluate(
+      ($) => ($ as HTMLImageElement).getAttribute('src')!,
+    )
+  }
+
+  let result: Result = {
+    id,
+    name,
+    url,
+  }
+
+  if (imageUrl) {
+    result.imageUrl = imageUrl
+  }
+
+  return result
+}
 
 const facebook: Input<Options> = (options: Options) => async () => {
   let results: Result[] = []
@@ -27,40 +92,7 @@ const facebook: Input<Options> = (options: Options) => async () => {
 
     results = _.reject(
       await Promise.all(
-        _.map(
-          await page.$$('._1xnd > div'),
-          async ($post): Promise<Result | undefined> => {
-            const $description = await $post.$(
-              '.userContent span span:nth-child(2)',
-            )
-            const $timestamp = await $post.$('[data-testid="story-subtitle"] a')
-
-            if (!$description || !$timestamp) {
-              return undefined
-            }
-
-            let url = await $timestamp.evaluate(
-              ($) => ($ as HTMLAnchorElement).href,
-            )
-            url = url.replace(/\?.*/, '')
-
-            const segments = _.split(new URL(url).pathname, '/')
-
-            if (segments[1] === 'events') {
-              return undefined
-            }
-
-            const id = _.last(segments)!
-
-            const name = await $description!.evaluate(($) => $.textContent!)
-
-            return {
-              id,
-              name,
-              url,
-            }
-          },
-        ),
+        _.map(await page.$$(`${POSTS_SELECTOR} > div`), parseResult),
       ),
       _.isUndefined,
     ) as Result[]
