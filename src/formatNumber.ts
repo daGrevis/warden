9a3d4d26a0e38672f376518827118cfbde682d51
Raitis Stengrevics <dagrevis@gmail.com>
2020-12-08 01:13:03 +0200

    Format floats as percentage

--- src/formatNumber.ts
+++ src/formatNumber.ts
@@ -1,6 +1,8 @@
+import _ from 'lodash'
+
 const formatNumber = (number: number) => {
   if (number % 1 !== 0) {
-    return `${number}`
+    return `${_.round(number * 100, 2)}%`
   }
 
   // https://stackoverflow.com/a/2901298/458610
